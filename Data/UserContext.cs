b6634cee19cf67799f356400fe33d743bacbc7ab
Daniel Thomas <dan@mac.home>
2026-01-28 16:33:28 +0000

    Added JWT Authentication Endpoints

--- Data/UserContext.cs
+++ Data/UserContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using ZeroAPI.Models;
 
-class UserContext : DbContext
+namespace ZeroAPI.Data
+{
+    public class UserContext : DbContext
 {
     public UserContext(DbContextOptions<UserContext> options) : base(options){}
     public DbSet<User> Users => Set<User>();
+}
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,43 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using ZeroAPI.DTOs;
 using ZeroAPI.Models;
+using ZeroAPI.Data;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<UserContext>(opt => opt.UseSqlite("Data Source=users.db"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+{
+    ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+    ValidAudience = builder.Configuration["JwtConfig:Audience"],
+    IssuerSigningKey = new SymmetricSecurityKey(
+        System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:Key"]!)
+    ),
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    NameClaimType = JwtRegisteredClaimNames.UniqueName
+};
+ 
+});
+builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
@@ -26,8 +58,18 @@ RouteGroupBuilder users = app.MapGroup("/users");
 users.MapGet("/", GetAllUsers);
 users.MapPost("/register", RegisterUser);
 users.MapPost("/login", UserLogin);
+users.MapPost("/login-jwt", JTWUserLogin);
+users.MapGet("/account-jwt", (ClaimsPrincipal user) =>
+{
+    var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+    var username = user.FindFirstValue(ClaimTypes.Name);
 
-app.UseStaticFiles();
+    return Results.Ok(new { userId, username });
+})
+.RequireAuthorization();
+
+RouteGroupBuilder jwtSess = app.MapGroup("jwt_sess");
+
 
 using (var scope = app.Services.CreateScope())
 {
@@ -35,7 +77,9 @@ using (var scope = app.Services.CreateScope())
     db.Database.EnsureCreated();
 }
 
-
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
 
 
@@ -70,7 +114,10 @@ static async Task<IResult> RegisterUser(UserCreateDTO userDTO, UserContext db)
     }
     
     userDTO = new UserCreateDTO(userAccount);
-    return TypedResults.Created($"ID: {userAccount.Id}, Name: {userAccount.Name}");
+    return Results.Created(
+    $"/users/{userAccount.Id}",
+    new UserDTO(userAccount)
+);
 }
 
 
@@ -94,4 +141,66 @@ static async Task<IResult> UserLogin(UserCreateDTO userDTO, UserContext db)
         await db.SaveChangesAsync();
     }
     return Results.Ok($"You have successfully authenticated as {userDTO.Name}");    
+}
+
+
+static async Task<IResult> JTWUserLogin(
+    UserCreateDTO userDTO,
+    UserContext db,
+    IConfiguration configuration)
+{
+    if (string.IsNullOrWhiteSpace(userDTO.Name))
+        return Results.BadRequest("Username required");
+
+    if (string.IsNullOrWhiteSpace(userDTO.Password))
+        return Results.BadRequest("Password required");
+    var user = await db.Users
+        .AsTracking()
+        .SingleOrDefaultAsync(u => u.Name == userDTO.Name);
+
+    if (user is null)
+        return Results.Unauthorized();
+
+    var hasher = new PasswordHasher<User>();
+    var result = hasher.VerifyHashedPassword(
+        user,
+        user.Password!,
+        userDTO.Password
+    );
+
+    if (result == PasswordVerificationResult.Failed)
+        return Results.Unauthorized();
+
+    if (result == PasswordVerificationResult.SuccessRehashNeeded)
+    {
+        user.Password = hasher.HashPassword(user, userDTO.Password);
+        await db.SaveChangesAsync();
+    }
+
+    var claims = new[]
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+        new Claim(JwtRegisteredClaimNames.UniqueName, user.Name!),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+    var key = new SymmetricSecurityKey(
+        System.Text.Encoding.UTF8.GetBytes(configuration["JwtConfig:Key"]!)
+    );
+
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    var token = new JwtSecurityToken(
+        issuer: configuration["JwtConfig:Issuer"],
+        audience: configuration["JwtConfig:Audience"],
+        claims: claims,
+        expires: DateTime.UtcNow.AddMinutes(30),
+        signingCredentials: creds
+    );
+
+    return Results.Ok(new
+    {
+        token = new JwtSecurityTokenHandler().WriteToken(token),
+        expires = token.ValidTo
+    });
 }
